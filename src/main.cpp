00621e507ef543d670cea7c8de25e573a2e87ab3
diegohommer <daugsteel@gmail.com>
2025-04-15 22:05:36 -0300

    making more solid benchmark, adding multiple runs per graph to get average metrics

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,8 @@ int single_run_mode(Algorithm algo) {
 }
 
 int benchmark_mode(Algorithm algo, const char* input_path, const char* output_name) {
+    const int num_runs = 15;
+    
     // Read all .graph files from the input folder into memory
     std::vector<Graph> graphs;
     for (const auto& entry : std::filesystem::directory_iterator(input_path)) {
@@ -47,6 +49,14 @@ int benchmark_mode(Algorithm algo, const char* input_path, const char* output_na
             graphs.push_back(std::move(graph));
         }
     }
+
+    // Sort the graphs by the number of vertices first, then by the number of arcs if vertices are equal
+    std::sort(graphs.begin(), graphs.end(), [](const Graph& a, const Graph& b) {
+        if (a.get_total_vertices() == b.get_total_vertices()) {
+            return a.get_total_arcs() < b.get_total_arcs();  // Compare arcs if vertices are equal
+        }
+        return a.get_total_vertices() < b.get_total_vertices();  // Otherwise, compare vertices
+    });
 
     // Construct the output file path
     std::filesystem::path output_dir = "./data/outputs/";
@@ -63,9 +73,19 @@ int benchmark_mode(Algorithm algo, const char* input_path, const char* output_na
     
     // Run and log stats of each graph over the selected algorithm
     for (auto& graph : graphs) {
-        FordResult result = ford_fulkerson(graph, graph.get_source(), graph.get_sink(), algo, true);
-        GraphMetrics metrics = Metrics::compute_graph_metrics(graph, algo, result);
-        Logger::log_instance_stats(metrics, algo, output_file);
+        GraphMetrics total_metrics = {0};
+        for (int i = 0; i < num_runs; ++i) {
+            Graph copy_graph(&graph);
+            FordResult result = ford_fulkerson(copy_graph, copy_graph.get_source(), copy_graph.get_sink(), algo, true);
+            GraphMetrics run_metrics = Metrics::compute_graph_metrics(copy_graph, algo, result);
+            total_metrics.increment(run_metrics);
+        }
+        
+        // Average the accumulated metrics over the number of runs
+        total_metrics.average(num_runs);
+        
+        // Log the averaged metrics for this graph
+        Logger::log_instance_stats(total_metrics, algo, output_file);
     }
 
     return 0; 
